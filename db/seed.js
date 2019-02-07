61194df693be69ad2be83078ceea519284a069ee
vinnyA3 <vincent.aceto@gmail.com>
2019-02-07 16:47:54 -0500

    feat(seeder): account for new schema fields

--- db/seed.js
+++ db/seed.js
@@ -1,39 +1,56 @@
 const sqlite3 = require('sqlite3').verbose();
 const faker = require('faker');
 const path = require('path');
+const {
+  genRandomInt,
+  moneyDelimValues,
+  genDollarsCents,
+  getLightningDeal,
+  multiplePricing,
+  getUsedPrice,
+  protectionPlan,
+} = require('./seedHelpers');
+
 // init database connection
-const db = new sqlite3.Database(path.join(__dirname, './db.amazon-products'));
-
-const genRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const concatTwoValuesWith = delim => val1 => val2 => `${val1}${delim}${val2}`;
-
-const moneyDelimValues = concatTwoValuesWith('.');
+const db = new sqlite3.Database(path.join(__dirname, './db.products'));
 
 // generate the random data
 db.serialize(() => {
-  const seed = db.prepare(
-    'INSERT INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+  const seed1 = db.prepare(
+    'INSERT INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
   );
 
+  const seed2 = db.prepare('INSERT INTO productReviews VALUES (?, ?, ?)');
+
   for (let i = 1; i < 201; i++) {
-    const randBills = genRandomInt(1, 800);
-    const randCents = genRandomInt(10, 99);
     const randStockNum = genRandomInt(1, 900);
 
-    seed.run([
-      i,
-      faker.commerce.productName(),
-      moneyDelimValues(randBills)(randCents),
-      faker.company.companyName(),
-      randStockNum,
-      !!genRandomInt(0, 1), // type cast int to bool (!!<int>)
-      faker.company.companyName(),
-      !!genRandomInt(0, 1),
-    ]);
+    seed1.run(
+      [
+        i,
+        faker.commerce.productName(),
+        genDollarsCents(),
+        ...getLightningDeal(),
+        ...multiplePricing(),
+        faker.company.companyName(), // owning
+        faker.company.companyName(), // fulfilled
+        randStockNum,
+        !!genRandomInt(0, 1), // type cast int to bool (!!<int>) - prime
+        !!genRandomInt(0, 1), // returnable
+        !!genRandomInt(0, 1), // gift
+        !!genRandomInt(0, 1), // buy used
+        ...getUsedPrice(),
+        ...protectionPlan(),
+      ],
+      err => {
+        if (err) {
+          console.error(err);
+        }
+      }
+    );
+
+    if (i < 71) {
+      seed2.run([i, genRandomInt(0, 801), genRandomInt(0, 5)]);
+    }
   }
 });
