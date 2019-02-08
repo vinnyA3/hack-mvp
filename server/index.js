97edad188ea8b520a8d734635bd1c4f3192e70b6
vinnyA3 <vincent.aceto@gmail.com>
2019-02-08 11:10:41 -0500

    fix(index): second query return

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3000;
+const { genRandomInt } = require('../db/seedHelpers');
 const db = new sqlite3.Database(path.join(__dirname, '../db/db.products'));
 const app = express();
 
@@ -10,39 +11,51 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Everything is good, life is good',
-    status: 200,
-  });
-});
-
 app.get('/:id', (req, res) => {
   const { id } = req.params;
 
   if (!id) {
-    return res.status(400).send({
+    return res.send({
       message: 'Please supply an ID!',
       status: 400,
     });
   }
 
-  const product = db.prepare('SELECT * FROM products WHERE id = (?)');
+  // const product = db.prepare('SELECT * FROM products WHERE id = (?)');
+  db.serialize(() => {
+    db.get('SELECT * FROM products WHERE id = (?)', id, (err, rows) => {
+      if (err) {
+        return res.send({
+          message: 'Oops, something went wrong!!',
+          status: 500,
+        });
+      }
 
-  product.each(id, (err, rows) => {
-    if (err) {
-      return res.status(500).send({
-        message: 'Oops, something went wrong!!',
-        status: 500,
-      });
-    }
+      if (!!rows.protectionPlan) {
+        db.get(
+          'SELECT * FROM productReviews WHERE id = ?',
+          genRandomInt(0, 70),
+          (err, rRows) => {
+            if (err) {
+              return res.send({
+                message: 'Oops, something went wrong!!',
+                status: 500,
+              });
+            }
 
-    // TODO
-    // if successful, then lets query for the productReviews if the
-    // protectionPlan || pricingOption exists
-    res.status(200).send({
-      rows,
-      status: 200,
+            return res.send({
+              rows,
+              rRows,
+              status: 200,
+            });
+          }
+        );
+      } else {
+        return res.send({
+          rows,
+          status: 200,
+        });
+      }
     });
   });
 });
