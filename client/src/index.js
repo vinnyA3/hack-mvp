9c4a2dfa39aebb0388ad4cf4a43000f7af5a2bc2
vinnyA3 <vincent.aceto@gmail.com>
2019-02-09 13:25:47 -0500

    refactor(index): move cart into own component

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import styles from './global';
+import ProductCart from 'productCart';
+
+render(<ProductCart />, document.getElementById('app'));
