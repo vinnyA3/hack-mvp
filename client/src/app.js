01947dc52ecbf16f4d0e731ed4929bb975378078
vinnyA3 <vincent.aceto@gmail.com>
2019-02-09 10:45:00 -0500

    feat(app): make dummy request to server

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import { render } from 'react-dom';
+import styles from './styles';
 
-const App = () => <h1>Hello Hack Reactor!!</h1>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null,
+      productReviews: null,
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/23').then(({ data }) => {
+      this.setState({
+        product: data.rows,
+        productReviews: data.rRows,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <h1>Hello Hack Reactor!!</h1>
+        <div>
+          {this.state.product
+            ? `${JSON.stringify(this.state.product, null, 2)} ${JSON.stringify(
+                this.state.productReviews,
+                null,
+                2
+              )}`
+            : ''}
+        </div>
+      </div>
+    );
+  }
+}
 
 render(<App />, document.getElementById('app'));
