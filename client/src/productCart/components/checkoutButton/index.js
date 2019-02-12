646d321bec8165fbeda0d60d021676c77cc4b90f
vinnyA3 <vincent.aceto@gmail.com>
2019-02-12 01:01:17 -0500

    feat(checkout): add icon and wrap btn text in span

--- client/src/productCart/components/checkoutButton/index.js
+++ client/src/productCart/components/checkoutButton/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './styles.scss';
 
 const checkoutButton = () => {
-  return <button className={styles.button}>Add to Cart</button>;
+  return <button className={styles.button}><i className={styles.button__icon}></i><span>Add to Cart</span></button>;
 };
 
 export default checkoutButton;
