54a06757b1c9f0ac23ed339e19d857844cd3ae82
vinnyA3 <vincent.aceto@gmail.com>
2019-02-11 23:14:33 -0500

    feat(components): relocate components + styles

---
+++ client/src/productCart/components/checkoutButton/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import styles from './styles.scss';
+
+const checkoutButton = () => {
+  return <button className={styles.button}>Add to Cart</button>;
+};
+
+export default checkoutButton;
