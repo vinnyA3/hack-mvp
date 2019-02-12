becb39efe0a395ee066eeb9c5e3542cfc3d8a453
vinnyA3 <vincent.aceto@gmail.com>
2019-02-11 23:12:14 -0500

    feat(cart): inject components
    Inject necessary components, add model code, add if / else clauses for
    conditional component rendering

--- client/src/productCart/index.js
+++ client/src/productCart/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
 import axios from 'axios';
 import styles from './styles.scss';
+// general
+import DropdownSelect from 'components/dropdownSelect';
+import optionGenerator from 'utils/optionGenerator';
+import ProtectionPlan from './components/protectionPlan';
+import CheckoutButton from './components/checkoutButton';
 
 class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      showModal: false,
       product: null,
       productReviews: null,
     };
@@ -13,6 +20,7 @@ class Cart extends Component {
 
   componentDidMount() {
     axios.get('/api/23').then(({ data }) => {
+      console.log(data.rows);
       this.setState({
         product: data.rows,
         productReviews: data.rRows,
@@ -23,15 +31,36 @@ class Cart extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1>Hello Hack Reactor!!</h1>
         <div>
-          {this.state.product
-            ? `${JSON.stringify(this.state.product, null, 2)} ${JSON.stringify(
-              this.state.productReviews,
-              null,
-              2
-            )}`
-            : ''}
+          {this.state.product ? (
+            <>
+              <h3 className={styles.price}>
+                <span className={styles.currency}>&#36;</span>
+                {this.state.product.originalPrice}
+              </h3>
+              {this.state.product.numInStock > 20 ? (
+                <h3 className={styles.inStock}>In Stock.</h3>
+              ) : (
+                'hello'
+              )}
+              <DropdownSelect type="Qty" children={optionGenerator(30)} />
+              {this.state.product.protectionPlan ? (
+                <ProtectionPlan
+                  protectionPlanPricingOptionOne={
+                    this.state.product.protectionPlanPricingOptionOne
+                  }
+                  protectionPlanPricingOptionTwo={
+                    this.state.product.protectionPlanPricingOptionTwo
+                  }
+                />
+              ) : (
+                ''
+              )}
+              <CheckoutButton />
+            </>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     );
