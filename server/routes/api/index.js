268a210448be7366cae6757d854f1951b1e87b79
vinnyA3 <vincent.aceto@gmail.com>
2019-02-14 17:02:18 -0500

    feat(api): inject cacheMiddleware

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { cacheMiddleware } = require('middleware');
 const { getGeolocation, getProductInfo } = require('controllers/api');
 
-router.route('/location').get(getGeolocation);
+router
+  .route('/location')
+  .all(cacheMiddleware(600))
+  .get(getGeolocation);
+
 router.route('/:id').get(getProductInfo);
 
 module.exports = router;
