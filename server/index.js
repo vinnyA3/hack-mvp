83de28684502b8595d5aa79bcd845d5fffbc2bba
vinnyA3 <vincent.aceto@gmail.com>
2019-02-14 16:47:18 -0500

    style(server): remove new lines

--- server/index.js
+++ server/index.js
@@ -3,16 +3,12 @@ const morgan = require('morgan');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
-
 // MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '../public')));
-
 // ROUTES
 app.use('/api', require('./routes/api'));
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
