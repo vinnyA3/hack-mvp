3750dfe6b7316f769682695cdd0245248add6afe
vinnyA3 <vincent.aceto@gmail.com>
2019-02-16 16:54:55 -0500

    feat: remove extra button files

--- client/src/productCart/components/checkoutButton/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import styles from './styles.scss';
-
-const checkoutButton = () => {
-  return <button className={styles.button}><i className={styles.button__icon}></i><span>Add to Cart</span></button>;
-};
-
-export default checkoutButton;
