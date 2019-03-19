0f1ae8a75a14befe9bc60a271dcb5cea564b614e
vinnyA3 <vincent.aceto@gmail.com>
2019-03-19 13:30:19 -0400

    feat(App): add id route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
 import Navigation from 'components/navigation';
 import Home from 'pages/Home';
 import VideoRoom from 'pages/VideoRoom';
 import { container, main, sidebar, header, footer } from './app.scss';
+// init font awesome
+// library.add(faIgloo)
 
 const App = () => (
   <Router>
@@ -13,7 +18,7 @@ const App = () => (
       </header>
 
       <Route path="/" exact component={Home} />
-      <Route path="/video" component={VideoRoom} />
+      <Route path="/video/:id" component={VideoRoom} />
 
       <footer className={footer}>
         <h2>Hello there</h2>
