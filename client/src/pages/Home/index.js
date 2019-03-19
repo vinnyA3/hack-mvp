38e51d90796c0108e0229f4d6e0c2b9aafeadc16
vinnyA3 <vincent.aceto@gmail.com>
2019-03-19 13:32:18 -0400

    refactor(home): functional to class component

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { container } from './styles';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Home = () => (
-  <div className={container}>
-    <h1>Hello from Home!!</h1>;
-  </div>
-);
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rooms: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/lobby').then(({ data }) => {
+      console.log(data.data);
+      this.setState({
+        rooms: data.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className={container}>
+        {this.state.rooms.length > 0
+          ? this.state.rooms.map(room => {
+              return <Link to={'/video/' + room._id}>{room.roomname}</Link>;
+            })
+          : ''}
+      </div>
+    );
+  }
+}
 
 export default Home;
