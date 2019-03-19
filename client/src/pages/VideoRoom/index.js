01862c40a54ebad8cffea0cda24f2fd72c1f2bf2
vinnyA3 <vincent.aceto@gmail.com>
2019-03-18 22:48:04 -0400

    feat(videoRoom): create stateful component

--- client/src/pages/VideoRoom/index.js
+++ client/src/pages/VideoRoom/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { container } from './styles';
 
 const VideoRoom = () => (
@@ -6,5 +6,36 @@ const VideoRoom = () => (
     <h1>Hello from VideoRoom!!</h1>
   </div>
 );
+
+const renderChat = ({ chat_messages }) => {
+  return chat_messages.map(({ username, text }) => {
+    return (
+      <div>
+        <span>{username}</span>
+        <p>{text}</p>
+      </div>
+    );
+  });
+};
+
+class VideoRoom extends Component {
+  constructor() {
+    super();
+    this.state = {
+      video_url: null,
+      chat_messages: [],
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.chat_messages.length > 0
+          ? renderChat(this.state.chat_messages)
+          : ''}
+      </Fragment>
+    );
+  }
+}
 
 export default VideoRoom;
